a5aba05b81303ae5786169a8899193dc8da43208
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-09 15:20:45 +0200

    collectd install script modified

--- collectd/install-collectd.sh
+++ collectd/install-collectd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo -ne "entering /tmp ..."
+echo "entering /tmp ..."
 cd /tmp 
 # Just to make sure you have the c-compiler linker and co
 
 echo -ne "installing build-essential ..."
-apt-get install build-essential
+apt-get install build-essential > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE"; fi
 
 ## Plugin dependencies instalttion
@@ -14,13 +14,13 @@ if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE"; fi
 #apt-get install libcurl4-gnutls-dev librtmp-dev
 
 #Dependencies for the write_mongo plugin
-echo -ne "installing dependencies for write_mongo ..."
+echo "installing dependencies for write_mongo ..."
 
 echo -ne "cloning mongoc project ..."
 git clone https://github.com/mongodb/mongo-c-driver.git libmongoc > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
 
-echo -ne "entering libmongoc ..."
+echo "entering libmongoc ..."
 cd ligmongoc
 
 echo -ne "checking v.0.7.1 version ..."
@@ -43,14 +43,14 @@ echo -ne "create symbolic link for libbson in /lib/ ..."
 ln -sf /usr/local/lib/libbson.so.0.7 /lib/ > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
 
-echo -ne "exiting libmongoc ..."
+echo "exiting libmongoc ..."
 cd ..
 
-echo -ne "installing dependencies for write_mongo ...OK"
+echo "installing dependencies for write_mongo ...OK"
 
 
 #Get collectd and install it
-echo -ne "installing collectd ..."
+echo "installing collectd ..."
 
 echo -ne "fetching sources files from collectd.org ..."
 wget http://collectd.org/files/collectd-5.3.0.tar.bz2 > /dev/null 2> /tmp/collectd.log
@@ -60,7 +60,7 @@ echo -ne "decompressing files ..."
 tar jxf collectd-5.3.0.tar.bz2 > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
 
-echo -ne "entering collectd-x.y.z ..."
+echo "entering collectd-x.y.z ..."
 cd collectd-5.3.0
 
 echo -ne "configuring collectd ..."
@@ -71,6 +71,6 @@ echo -ne "installing collectd ..."
 make all install > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
 
-echo -ne "installing collectd ...OK"
+echo "installing collectd ...OK"
 
 
