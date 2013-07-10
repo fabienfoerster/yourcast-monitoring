11964f64416e44b173058b6b35bfaa83212db38e
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-10 09:48:41 +0200

    delete useless part of script

--- jetty/configure-jetty.sh
+++ jetty/configure-jetty.sh
@@ -13,10 +13,6 @@ echo -ne "replacing jetty-jmx.xml ..."
 mv jetty-jmx.xml "$1/etc/jetty-jmx.xml"
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/jetty.log; fi
 
-echo -ne "adding jetty-jmx.xml to start.ini ..."
-echo "etc/jetty-jmx.xml" >> "$1/start.ini"
-if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/jetty.log; fi
-
 echo "Done"
 echo "For error see above."
 
