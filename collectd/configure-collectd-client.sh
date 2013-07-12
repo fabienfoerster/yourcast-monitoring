620432bf64dc49c4d10c47666dc591edbafd74f7
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-12 14:36:40 +0200

    script install/configure for client/monitor

---
+++ collectd/configure-collectd-client.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+usage() {
+    echo "usage: $0 -s server_name -p server_port -i interval"
+    echo "-s server_name : the name of the server you want to send the collected data"
+    echo "-p server_port : the port of the server you want to send the collected data"
+    echo "-p interval : the frequency you want to collect the data"
+}
+
+server_name=""
+server_port=""
+interval=""
+
+while getopts ":hs:p:i:" option; do
+    case "$option" in 
+        s) server_name="$OPTARG" ;;
+        p) server_port="$OPTARG" ;;
+        i) interval="$OPTARG" ;;
+        :)  echo "Error: -$OPTARG requires an argument" 
+            usage
+            exit 1            
+            ;;
+        h) usage
+           exit 0
+           ;;
+        ?)  echo "Error: unknown option -$OPTARG" 
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$server_name" ]; then
+    echo "Error : the server_name argument must be specify"
+    usage
+    exit 1
+fi
+
+if [ -z "$server_port" ]; then
+    echo "Error : the server_port argument must be specify"
+    usage
+    exit 1
+fi
+
+if [ -z "$interval" ]; then
+    echo "Error : the interval argument must be specify"
+    usage
+    exit 1
+fi
+
+sed -i "s/{{server_name}}/$server_name/" collectd.conf
+sed -i "s/{{server_port}}/$server_port/" collectd.conf
+sed -i "s/{{interval}}/$interval/" collectd.conf 
+
---
+++ collectd/install-collectd-client.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+usage() {
+    echo "usage: $0 -j jdk_directory "
+}
+
+dir_jdk=""
+
+while getopts ":hj:" option; do
+    case "$option" in 
+        j) dir_jdk="$OPTARG" ;;
+        :)  echo "Error: -$OPTARG requires an argument" 
+            usage
+            exit 1            
+            ;;
+        h) usage
+           exit 0
+           ;;
+        ?)  echo "Error: unknown option -$OPTARG" 
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+if [ ! -d "$dir_jdk" ]; then
+    echo "Error : the jdk_directory argument must be a directory"
+    usage
+    exit 1
+fi
+
+
+echo "entering /tmp"
+cd /tmp 
+
+echo -ne "installing build-essential ..."
+apt-get install build-essential > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+
+#Dependencies for the write_http plugin
+#apt-get install libcurl4-gnutls-dev librtmp-dev
+
+
+echo "installing collectd ..."
+
+echo -ne "fetching sources files from collectd.org ..."
+wget http://collectd.org/files/collectd-5.3.0.tar.bz2 > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo -ne "decompressing files ..."
+tar jxf collectd-5.3.0.tar.bz2 > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo "entering collectd-x.y.z"
+cd collectd-5.3.0
+
+echo -ne "configuring collectd(can take a while) ..."
+./configure --with-java="$dir_jdk" > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo -ne "installing collectd(can take a while) ..."
+make all install > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo "exiting collectd-x.y.z"
+cd ..
+
+echo -ne "deleting collectd-x.y.z ..."
+rm -rf collectd-5.3.0
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo -ne "deleting collectd-x.y.z.tar.bz2 ..."
+rm -rf collectd-5.3.0.tar.bz2
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo -ne "fetching types.db.custom ..."
+wget https://raw.github.com/fabienfoerster/yourcast-monitoring/master/collectd/config/types.db.custom > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi
+
+echo -ne "replacing types.db.custom ..."
+mv types.db.custom /opt/collectd/share/collectd/types.db.custom
+if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log; fi 
