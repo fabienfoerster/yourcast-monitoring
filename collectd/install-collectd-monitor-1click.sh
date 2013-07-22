f5f06c71e24d10a5ad06127f8615faeeddcc8480
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-22 09:27:47 +0200

    add support fedora

--- collectd/install-collectd-monitor-1click.sh
+++ collectd/install-collectd-monitor-1click.sh
@@ -174,11 +174,16 @@ if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exi
 
 echo -n "adding script to boot sequence ... "
 update-rc.d collectd defaults > /dev/null 2> /tmp/collectd.log
+if [ "$?" = "0" ]; then echo "OK"; 
+else 
+chkconfig --level 123456 collectd > /dev/null 2> /tmp/collectd.log
+if [ "$?" != "0" ]; then echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
+chkconfig --add collectd > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
+fi
 
 echo -n "starting collectd ..."
 /opt/collectd/sbin/collectd > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
 
 echo "Done"
-
