c3b37786daefe80f049c7f733b843e30bebad509
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-23 10:32:12 +0200

    add missing parameter

--- collectd/remove-collectd-client.sh
+++ collectd/remove-collectd-client.sh
@@ -16,7 +16,7 @@ echo -n "removing collectd from boot sequence ..."
 update-rc.d -f  collectd remove > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; 
 else 
-chkconfig --del > /dev/null 2> /tmp/collectd.log
+chkconfig --del collectd > /dev/null 2> /tmp/collectd.log
 fi
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
 
