7012e259e7c38f3a4d5188c7cdc15e797b187006
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-23 10:38:03 +0200

    fix

--- collectd/install-collectd-client-1click.sh
+++ collectd/install-collectd-client-1click.sh
@@ -79,6 +79,8 @@ else
     yum groupinstall “Development Tools” > /dev/null 2> /tmp/collectd.log
     yum install python > /dev/null 2> /tmp/collectd.log
     yum install python-devel > /dev/null 2> /tmp/collectd.log
+    easy_install pip > /dev/null 2> /tmp/collectd.log
+    pip install pymongo > /dev/null 2> /tmp/collectd.log
 fi
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
 
