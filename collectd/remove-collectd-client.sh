d32674aa8c8d3bc8a15626788078d08f51d7da46
Fabien Foerster <fabienfoerster@gmail.com>
2013-07-23 10:55:58 +0200

    fix double OK

--- collectd/remove-collectd-client.sh
+++ collectd/remove-collectd-client.sh
@@ -17,8 +17,8 @@ update-rc.d -f  collectd remove > /dev/null 2> /tmp/collectd.log
 if [ "$?" = "0" ]; then echo "OK"; 
 else 
 chkconfig --del collectd > /dev/null 2> /tmp/collectd.log
-fi
 if [ "$?" = "0" ]; then echo "OK"; else echo "FAILURE";cat /tmp/collectd.log;exit 1; fi
+fi
 
 echo -n "removing service script ..."
 rm /etc/init.d/collectd > /dev/null 2> /tmp/collectd.log
